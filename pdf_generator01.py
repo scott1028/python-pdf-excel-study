bb114cec11e26e73ac3fb572ac0e3c5bb9a9d123
scott <mic1028002@gmail.com>
2019-01-19 00:26:38 +0800

    update code

--- pdf_generator01.py
+++ pdf_generator01.py
@@ -1,28 +1,20 @@
 from weasyprint import HTML, CSS
 from weasyprint.fonts import FontConfiguration
+from jinja2 import Template
 
 font_config = FontConfiguration()
-html = HTML(string='''
-  <html>
-    <h1>The title</h1>
-    <body>
-      <div>
-        <div>
-          <div width="100" style="border: 1px solid coral;">Test</div>
-          <div width="100" style="border: 1px solid coral;">Test1</div>
-        </div>
-        <div>
-          <div width="100" style="border: 1px solid coral;">Test</div>
-          <div width="100" style="border: 1px solid coral;">Test</div>
-        </div>
-        <div>
-          <div width="100" style="border: 1px solid coral;">Test</div>
-          <div width="100" style="border: 1px solid coral;">Test</div>
-        </div>
-      </div>
-    </body>
-  </html>
-''')
+
+t = Template('{% for item in items%}<div width="100" style="border: 1px solid coral;">{{ item }}</div>{% endfor %}')
+
+output = '''
+<div>
+  %s
+</div>
+''' % t.render(items=[1,2,3,4,5,6])
+
+print(output)
+
+html = HTML(string=output)
 css = CSS(string='''
     @page {
       size: A4; /* Change from the default size of A4 */
